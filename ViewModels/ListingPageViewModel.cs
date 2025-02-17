50506d67e0c13eab77ccc5fcbde31fc54876fad1
Stom <Stom@COMPUTER>
2025-02-17 04:42:14 +0200

    fd

--- ViewModels/ListingPageViewModel.cs
+++ ViewModels/ListingPageViewModel.cs
@@ -1,12 +1,49 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MedicineClient.Services;
+using MedicineClient.Models;
 
 namespace MedicineClient.ViewModels
 {
-    public class ListingPageViewModel
+    public class ListingPageViewModel : ViewModelBase
     {
+        MedicineWebApi proxy;
+        ServiceProvider serviceProvider;
+        public ListingPageViewModel(MedicineWebApi proxy, ServiceProvider serviceProvider)
+        {
+            this.proxy= proxy;
+            this.serviceProvider= serviceProvider;
+            temp = new List<Medicine>();
+            GetMedicineList();
+            Listing = new ObservableCollection<Medicine>(temp);
+            OnRefresh = new Command(Refresh);
+            IsRefreshing = false;
+        }
+        private ObservableCollection<Medicine> listing;
+        public ObservableCollection<Medicine> Listing
+        {
+            get => listing; set { listing = value; OnPropertyChanged(); }
+        }
+        private List<Medicine> temp;
+        private async void GetMedicineList()
+        {
+            temp=proxy.GetMedicineList().Result;
+        }
+        public Command OnRefresh { get; }
+        private async void Refresh()
+        {
+            if (!IsRefreshing)
+            {
+                IsRefreshing = true;
+                GetMedicineList();
+                IsRefreshing= false;
+                Listing=new ObservableCollection<Medicine>(temp);
+            }
+        }
+        public bool IsRefreshing;
     }
 }
